fa2ebf50373120956594fc816f8ba55ec8a8e213
Gavin Eke <GavinEke@users.noreply.github.com>
2017-10-31 20:36:52 +1100

    Attempt Pester Test

--- TravisCI.ps1
+++ TravisCI.ps1
@@ -1,3 +1,4 @@
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name Pester, PSScriptAnalyzer -SkipPublisherCheck -Scope CurrentUser -Force
+Invoke-Pester @Verbose -Path './Tests'
 # More to after tests created... GH Issue #1
