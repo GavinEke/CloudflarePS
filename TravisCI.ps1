8120d7c45347828e590db99cff8dfc548d1da02f
Gavin Eke <GavinEke@users.noreply.github.com>
2017-11-02 16:50:05 +1100

    Update TravisCI.ps1

--- TravisCI.ps1
+++ TravisCI.ps1
@@ -1,4 +1,6 @@
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name Pester, PSScriptAnalyzer -SkipPublisherCheck -Scope CurrentUser -Force
-Invoke-Pester -Path './Tests' -EnableExit
+#Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+#Install-Module -Name Pester, PSScriptAnalyzer -SkipPublisherCheck -Scope CurrentUser -Force
+#Invoke-Pester -Path './Tests' -EnableExit
 # More to after tests created... GH Issue #1
+Get-ChildItem Env:
+Get-ChildItem Variable:
