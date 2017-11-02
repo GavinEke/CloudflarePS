e0a8f7208e50214cb85e8007b27babf1e5ffd42a
Gavin Eke <GavinEke@users.noreply.github.com>
2017-11-03 09:55:31 +1100

    Test BuildHelpers

--- TravisCI.ps1
+++ TravisCI.ps1
@@ -1,6 +1,7 @@
-#Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 #Install-Module -Name Pester, PSScriptAnalyzer -SkipPublisherCheck -Scope CurrentUser -Force
 #Invoke-Pester -Path './Tests' -EnableExit
 # More to after tests created... GH Issue #1
-Get-ChildItem Env:
-Get-ChildItem Variable:
+Install-Module -Name BuildHelpers -SkipPublisherCheck -Scope CurrentUser -Force
+Get-BuildVariables
+Get-ProjectName
