14968d3b7227d769c7ea5cad57f7265f23cc2bb7
Gavin Eke <gavineke-github@yahoo.com.au>
2017-11-05 16:31:42 +1100

    minor fix to spacing

--- Tests/CloudflarePS.Tests.ps1
+++ Tests/CloudflarePS.Tests.ps1
@@ -10,14 +10,14 @@ Describe "$ModuleName Module" {
     Context 'Strict mode' {
         Set-StrictMode -Version latest
 		
-		It 'should not have any PSScriptAnalyzer warnings' {
+        It 'should not have any PSScriptAnalyzer warnings' {
             Import-Module PSScriptAnalyzer -Force -ErrorAction SilentlyContinue
             $ScriptWarnings = Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\$ModuleName" -Recurse -ExcludeRule PSUseShouldProcessForStateChangingFunctions
             $ScriptWarnings | Should BeNullOrEmpty
-		}
+        }
 		
         It "has a valid manifest" {
             { Test-ModuleManifest -Path "$PSScriptRoot\..\$ModuleName\$ModuleName.psd1" -ErrorAction Stop -WarningAction SilentlyContinue } | Should Not Throw
-		}
+        }
     }
 }
