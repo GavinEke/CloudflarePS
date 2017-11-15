5aac51a6bd82e639438c3dbb6f9f0b7018c81396
Gavin Eke <GavinEke@users.noreply.github.com>
2017-11-16 05:26:56 +1100

    Pester 4.1 is now out

--- TravisCI.ps1
+++ TravisCI.ps1
@@ -1,7 +1,4 @@
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-#Install-Module -Name Pester, PSScriptAnalyzer -SkipPublisherCheck -Scope CurrentUser -Force
-#Invoke-Pester -Path './Tests' -EnableExit
+Install-Module -Name Pester, PSScriptAnalyzer -SkipPublisherCheck -Scope CurrentUser -Force
+Invoke-Pester -Path './Tests' -EnableExit
 # More to after tests created... GH Issue #1
-Install-Module -Name BuildHelpers -SkipPublisherCheck -Scope CurrentUser -Force
-Get-BuildVariables
-Get-ProjectName
