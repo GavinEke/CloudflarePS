8c3839c91c67d005881f06a4671dbda411f46bb8
Gavin Eke <GavinEke@users.noreply.github.com>
2017-11-24 11:57:22 +1100

    Upgrade build system to psake

--- TravisCI.ps1
+++
@@ -1,4 +0,0 @@
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name Pester, PSScriptAnalyzer -SkipPublisherCheck -Scope CurrentUser -Force
-Invoke-Pester -Path './Tests' -EnableExit
-# More to after tests created... GH Issue #1
